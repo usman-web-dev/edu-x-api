0324a0b39bb1f6eaf06e4cc4536b40bbc8662cba
Usman Javed <usman.javed@coeus-solutions.de>
2022-05-27 02:12:20 +0500

    Modify filters

--- src/api/course-assignment/controllers/course-assignment.js
+++ src/api/course-assignment/controllers/course-assignment.js
@@ -1,9 +1,34 @@
-'use strict';
+"use strict";
+const { merge } = require("lodash");
 
 /**
  *  course-assignment controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::course-assignment.course-assignment');
+module.exports = createCoreController(
+  "api::course-assignment.course-assignment",
+  () => ({
+    async find(ctx) {
+      ctx.query = merge(ctx.query, {
+        filters: {
+          section: {
+            class: {
+              batch: {
+                department: {
+                  grade: {
+                    institute: { id: { $eq: ctx.state.user.institute.id } },
+                  },
+                },
+              },
+            },
+          },
+        },
+      });
+
+      const { data, meta } = await super.find(ctx);
+      return { data, meta };
+    },
+  })
+);
