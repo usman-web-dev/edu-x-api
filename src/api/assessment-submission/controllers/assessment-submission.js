e1b635599bb5ee0b25320f20fb1a52af4cf8de5a
Usman Javed <usman.javed@coeus-solutions.de>
2022-05-30 04:32:52 +0500

    Setup assessments.

--- src/api/assessment-submission/controllers/assessment-submission.js
+++ src/api/assessment-submission/controllers/assessment-submission.js
@@ -1,9 +1,40 @@
-'use strict';
+"use strict";
+const {
+  errors: { UnauthorizedError },
+} = require("@strapi/utils");
 
 /**
  *  assessment-submission controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::assessment-submission.assessment-submission');
+module.exports = createCoreController(
+  "api::assessment-submission.assessment-submission",
+  () => ({
+    async update(ctx) {
+      const { data, meta } = await super.find(ctx);
+
+      const {
+        student: { id },
+        assessment: { type, subType },
+        obtainedMarks,
+      } = ctx.request.body.data;
+
+      if (obtainedMarks != null && ctx.state.user.role === 4) {
+        throw new UnauthorizedError(
+          "You are not allowed to mark the assessment."
+        );
+      }
+
+      await strapi.entityService.create("api::notification.notification", {
+        data: {
+          users: [id],
+          text: `${ctx.state.user.username} has marked your assessment (${type}-${subType})`,
+        },
+      });
+
+      return { data, meta };
+    },
+  })
+);
