6198cdc810ef919c625393ffbc8109e455206f7a
Usman Developer <usman.developer002@gmail.com>
2023-03-16 12:13:49 +0500

    revert db envs

--- config/database.js
+++ config/database.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: "mysql",
     connection: {
-      host: env("MYSQLHOST", "127.0.0.1"),
-      port: env.int("MYSQLPORT", 3306),
-      database: env("MYSQLDATABASE", "edu-x"),
-      user: env("MYSQLUSER", "root"),
-      password: env("MYSQLPASSWORD", ""),
-      ssl: env.bool("MYSQLSSL", false),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 3306),
+      database: env("DATABASE_NAME", "edu-x"),
+      user: env("DATABASE_USERNAME", "root"),
+      password: env("DATABASE_PASSWORD", ""),
+      ssl: env.bool("DATABASE_SSL", false),
     },
   },
 });
